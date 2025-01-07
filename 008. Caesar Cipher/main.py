a6a9e0e9eb49a7e0ac12750a39e9209d16867d94
Mohammad Safri Dwi Rizky <74010926+safridwirizky@users.noreply.github.com>
2025-01-07 09:05:35 +0700

    Update main.py

--- 008. Caesar Cipher/main.py
+++ 008. Caesar Cipher/main.py
@@ -12,7 +12,9 @@ def caesar(original_text, shift_amount, encode_or_decode):
     global flag
 
     for letter in original_text:
-        if encode_or_decode == "decode":
+        if letter not in alphabet:
+            output_text += letter
+        elif encode_or_decode == "decode":
             shifted_position = alphabet.index(letter) - shift_amount
             shifted_position %= len(alphabet)
             output_text += alphabet[shifted_position]
