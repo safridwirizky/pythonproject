4bdc4f13a605eccc16ad62bf6cd94f5e1ef8247b
safridwirizky <safridwirizky@gmail.com>
2026-03-03 13:26:41 +0800

    Beautiful Soup

--- day_45/main.py
+++ day_45/main.py
@@ -3,8 +3,23 @@ import requests
 
 URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
-response = requests.get(url=URL)
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+}
+
+response = requests.get(url=URL, headers=headers)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-print(soup.prettify())
+titles = soup.select("span.content_content__i0P3p h2 strong")
+
+movies = []
+for title in titles:
+    text = title.get_text()
+    if ")" not in text:
+        continue
+    movies.append(text)
+
+with open('movies.txt', 'w', encoding='utf-8') as file:
+    for movie in movies[::-1]:
+        file.write(movie + '\n')
