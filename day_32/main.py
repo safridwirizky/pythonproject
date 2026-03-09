eb045fae4437a357cdc894e716b9c9deb32e8a5e
safridwirizky <safridwirizky@gmail.com>
2026-03-09 09:02:11 +0800

    update

--- day_32/main.py
+++ day_32/main.py
@@ -1,9 +1,14 @@
-import smtplib
+import smtplib, os
 
-my_mail = "ujicobamailpython772@gmail.com"
-password = "123"
+my_mail = "prjctpy@gmail.com"
+password = os.getenv("PRJCTPY_PASSWORD")
 
-connection = smtplib.SMTP("smtp.google.com")
+connection = smtplib.SMTP("smtp.gmail.com", port=587)
 connection.starttls()
-connection.mail()
+connection.login(user=my_mail, password=password)
+connection.sendmail(
+    from_addr=my_mail,
+    to_addrs="zz_zz93@yahoo.com",
+    msg="Subject:Hello Safri\n\nAku lagi coba smtp."
+)
 connection.close()
