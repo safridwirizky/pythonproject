7a4aba742df8ea1a48796107d5f61590311a65d7
safridwirizky <safridwirizky@gmail.com>
2026-03-17 11:27:33 +0800

    Contact Me

---
+++ day_60/main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request
+import requests, smtplib, os
+
+my_email = os.getenv('MY_EMAIL')
+my_password = os.getenv('MY_PASSWORD')
+to_email = 'email@gmail.com'
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+@app.route('/contact', methods=['POST', 'GET'])
+def form_entry(): 
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+
+        h1 = 'Successfully sent your message.'
+        subheading = ''
+
+        print(my_email + my_password)
+
+        with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(
+                from_addr= my_email,
+                to_addrs= to_email,
+                msg= f'Subject: Contact\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}'
+                )
+
+        return render_template('contact.html', h1=h1, subheading=subheading)
+    
+    elif request.method == 'GET':
+        h1 = 'Contact Me'
+        subheading = 'Have questions? I have answers.'
+        return render_template('contact.html', h1=h1, subheading=subheading)
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
